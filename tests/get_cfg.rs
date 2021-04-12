b5ed0a743e5c78a160349205814a9a3203b01406
arctic_hen7 <arctic_hen7@pm.me>
2021-04-12 15:24:55 +1000

    refactor: ♻️ simplified some booleans and removed unused imports

--- tests/get_cfg.rs
+++ tests/get_cfg.rs
@@ -1,12 +1,10 @@
 use bonnie_lib::get_cfg;
 
-use std::env::VarError;
-
 #[test]
 fn returns_cfg_string() {
 	let cfg_path = "src/bonnie.toml";
 	let cfg = get_cfg(cfg_path);
-	if !cfg.is_ok() {
+	if cfg.is_err() {
 		// This is a super simple function, but it depends on a file existing, tell the user so they can create it if necessary!
 		panic!("Returned error on valid config file path. Please ensure 'src/bonnie.toml' still exists.");
 	}
--- tests/run_cmd.rs
+++ tests/run_cmd.rs
@@ -1,14 +1,12 @@
 use bonnie_lib::run_cmd;
 
-use std::env::VarError;
-
 // TODO test error cases here
 
 #[test]
 fn returns_empty() {
 	let cmd = "echo Test";
 	let output = run_cmd(String::from(cmd));
-	if !output.is_ok() {
+	if output.is_err() {
 		// This is a super simple function, but it depends on a file existing, tell the user so they can create it if necessary!
 		panic!("Returned error for valid command '{}'.", cmd);
 	}
@@ -18,7 +16,7 @@ fn returns_empty() {
 fn returns_empty_for_long_cmd() {
 	let cmd = "sleep 5 && echo Test";
 	let output = run_cmd(String::from(cmd));
-	if !output.is_ok() {
+	if output.is_err() {
 		// This is a super simple function, but it depends on a file existing, tell the user so they can create it if necessary!
 		panic!("Returned error for valid command '{}'.", cmd);
 	}
