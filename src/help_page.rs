6424fbb3332ad07769f80425c7b796ad6d1dadef
valentine-mario <valentine13400@gmail.com>
2021-04-25 08:59:49 +0100

    updated typo and contents in help page

--- src/help_page.rs
+++ src/help_page.rs
@@ -1,39 +1,35 @@
 // This file just defines a single constant for the help page that'll be served to users with `bonnie help`
 
-pub fn help_command() -> String {
-    let bonnie_help_page = format!("
+pub const BONNIE_HELP_PAGE: &str = "
 Example Usage:
-    Commands can be supplied by specifying in the bonnie.toml: 
+    Commands can be specified in bonnie.toml: 
 
         [script]
         greet.cmd = \"echo \"Greetings %lastname. I see your first name is %firstname?\"\"
 
-    arguments to the command can also be passed in the bonnie.toml eg
+        Arguments can be parsed into a command as well:
 
         [script]
-        greet.args = [\"firstname\",\"lastname\"]
+        greet.args = [\"firstname\",
+                     \"lastname\"]
 
     To run the command in the terimal:
         'bonnie greet Donald Knuth'
         
-    arguments can also be supplied multiple times in the command eg:
+    Arguments can be used multiple times in a command::
 
         [script]
         greet.cmd = \"echo \"Greetings %lastname. I see your first name is %firstname?\" and not %lastname\"
 
-Shorthand:
-    for commong scripts that require no args or cmd eg:
+    If a script doesn't need any arguments, you can use shorthand syntax:
 
         [scripts]
         foobar = \"echo Hello World\"
 
-    To execute, run 'bonnie foobar'
-Appending arguments:
-    To append arguments at the end of a bonnie script, you can do this easily in Bonnie by using shorthand syntax and adding a %% to the end of the command like so:
+    To run that, use the same syntax as before, just without any arguments: 'bonnie foobar'.
+    Appending arguments:
+        To append arguments at the end of a script, use shorthand syntax and add a '%%' to the end of the command:
         
         [scripts]
         dc = \"docker-compose --env-file .my.env %%\"
-");
-
-    bonnie_help_page
-}
+";
