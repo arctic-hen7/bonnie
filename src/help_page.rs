aeed110d56a423b618f9728caed1cce1015f0180
arctic_hen7 <arctic_hen7@pm.me>
2021-06-26 08:05:07 +1000

    docs: 📝 updated help page

--- src/help_page.rs
+++ src/help_page.rs
@@ -2,23 +2,25 @@
 
 pub const BONNIE_HELP_PAGE: &str = "
 Example Usage:
-    Commands can be specified in bonnie.toml: 
+    Commands can be specified in bonnie.toml:
 
-        [script]
-        greet.cmd = \"echo \"Greetings %lastname. I see your first name is %firstname?\"\"
+        [scripts]
+        greet.cmd = \"echo \\\"Greetings %lastname. I see your first name is %firstname?\\\"\"
 
         Arguments can be parsed into a command as well:
 
-        [script]
-        greet.args = [\"firstname\",
-                     \"lastname\"]
+        [scripts]
+        greet.args = [
+            \"firstname\",
+            \"lastname\"
+        ]
 
     To run the command in the terimal:
         'bonnie greet Donald Knuth'
-        
+
     Arguments can be used multiple times in a command:
 
-        [script]
+        [scripts]
         greet.cmd = \"echo \"Greetings %lastname. I see your first name is %firstname?\" and not %lastname\"
 
     If a script doesn't need any arguments, you can use shorthand syntax:
@@ -26,10 +28,29 @@ Example Usage:
         [scripts]
         foobar = \"echo Hello World\"
 
-    To run that, use the same syntax as before, just without any arguments: 'bonnie foobar'.
+    To run that, use the same syntax as before, just without any arguments:
+        'bonnie foobar'.
     Appending arguments:
         To append arguments at the end of a script, use shorthand syntax and add a '%%' to the end of the command:
-        
+
         [scripts]
         dc = \"docker-compose --env-file .my.env %%\"
+
+    Environment variables can also be inserted from custom files, like .env:
+
+        env_files = [
+            \".env\"
+        ]
+
+        [scripts]
+        interpolation.cmd = \"echo \\\"%GREETING %firstname!\\\"\"
+        interpolation.args = [
+            \"firstname\"
+        ]
+        interpolation.env_vars = [
+            \"GREETING\"
+        ]
+
+    To run that, use normal arguments syntax:
+        'bonnie interpolation Donald'.
 ";
