9c8ecac60d0a8bf6d48c7627ee2691a53b64c8db
arctic_hen7 <arctic_hen7@pm.me>
2021-06-28 12:21:40 +1000

    feat: ✨ added musl install script

---
+++ install_scripts/musl.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# This script is designed for Docker usage only! It must be run as root, and will install Bonnie directly to `/bin`.
+
+# Get the URL to the latest version of the musl binary
+url=$(curl -s https://api.github.com/repos/arctic-hen7/bonnie/releases/latest | grep 'browser_' | cut -d\" -f4 | grep musl)
+
+# Download the latest release of Bonnie for musl and put it in `/bin`
+curl -L $url > /bin/bonnie
