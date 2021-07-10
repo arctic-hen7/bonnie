bb36c38be56e8d89f16cf0007831ab88ea573f7b
Oscar Stevens <oscar_stevens@icloud.com>
2021-07-10 17:03:16 +1000

    feat(init): :sparkles: Add default templates
    This is used for `bonnie --init`. It fetches the file at `$BONNIE_TEMPLATE_PATH`, falling back to `~/.bonnie/template.toml` if necessary.  The `home` crate was introduced to handle the finding of the user's home directory on Windows, which is surprisingly complicated and **can fail**.

--- src/init.rs
+++ src/init.rs
@@ -1,10 +1,13 @@
+use crate::template;
 use crate::version::BONNIE_VERSION;
+
 use std::fs;
+use std::io;
 
 // Creates a new Bonnie configuration file using a template, or from the default
 pub fn init(template: Option<String>) -> Result<(), String> {
     // Check if there's already a config file in this directory
-    if fs::metadata("./bonnie.toml").is_ok() {
+    if fs::metadata("bonnie.toml").is_ok() {
         Err(String::from("A Bonnie configuration file already exists in this directory. If you want to create a new one, please delete the old one first."))
     } else {
         // Check if a template has been given
@@ -22,24 +25,29 @@ pub fn init(template: Option<String>) -> Result<(), String> {
             // We have a template file that doesn't exist
             return Err(format!("The given template file at '{}' does not exist or can't be read. Please make sure the file exists and you have the permissions necessary to read from it.", template.as_ref().unwrap()));
         } else {
-            // Create a new `bonnie.toml` file using the default
-            // TODO read the default from `~/.bonnie/template.toml` if it exists
-            output = fs::write(
-                "./bonnie.toml",
-                format!(
-                    "version=\"{version}\"
+            // get t
+            let template = match template::get_default() {
+                Ok(template) => Ok(template),
+                Err(template::Error::Other(err)) if err.kind() == io::ErrorKind::NotFound => {
+                    Ok(format!(
+                        "version=\"{version}\"
 
 [scripts]
 start = \"echo \\\"No start script yet!\\\"\"
-                ",
-                    version = BONNIE_VERSION
-                ),
-            );
+",
+                        version = BONNIE_VERSION
+                    ))
+                }
+                Err(err) => Err(err),
+            }
+            .map_err(|err| format!("{:#?}", err))?;
+
+            output = fs::write("bonnie.toml", template)
         }
 
         match output {
     		Ok(_) => Ok(()),
-    		Err(_) => Err(String::from("Error creating new bonnie.toml, make sure you have the permissions to write to this directory."))
+    		Err(_) => Err(format!("Error creating new bonnie.toml, make sure you have the permissions to write to this directory."))
     	}
     }
 }
---
+++ src/template.rs
@@ -0,0 +1,26 @@
+use home::home_dir;
+
+use std::env;
+use std::fs;
+use std::io;
+use std::path::PathBuf;
+
+#[derive(Debug)]
+pub enum Error {
+    CouldNotGetHomeDirectory,
+    Other(io::Error),
+}
+
+pub fn get_default() -> Result<String, Error> {
+    let default_template_path = home_dir()
+        .map(|x| x.join(".bonnie").join("template.toml"))
+        .ok_or(Error::CouldNotGetHomeDirectory)?;
+
+    let path = env::var("BONNIE_TEMPLATE_PATH")
+        .map(|x| PathBuf::from(x))
+        .unwrap_or(default_template_path);
+
+    let template = fs::read_to_string(path);
+
+    return template.map_err(|err| Error::Other(err));
+}
